efc7c83a952068d1b8fbcedfc842e1ae107d5240
Igor Guzek <ultimaa@gmail.com>
2022-04-11 22:45:03 +0200

    Added deleteValue and deleteExactPosition functions in ruleOf5 example to practice navigating the arrays.
    Some unifying formatting settings and clean up.

--- OopBasics/ruleOf5.cpp
+++ OopBasics/ruleOf5.cpp
@@ -1,107 +1,144 @@
 #include <iostream>
 #include <string>
 
-class NumbersContainer {
-    public:
+class NumbersContainer
+{
+public:
     using size_type = int;
     using value_type = int;
 
     NumbersContainer(size_type max_size)
         :max_size(max_size),
         count(0),
-        numbers(new value_type[max_size]){
+        numbers(new value_type[max_size])
+    {
     }
 
-    ~NumbersContainer(){
+    ~NumbersContainer()
+    {
         delete[] numbers;
         numbers = nullptr;
     }
 
-    NumbersContainer(const NumbersContainer &other)
-        : max_size(other.max_size),
-          count(other.count),
-          numbers(new value_type[max_size]){
-              for (value_type i = 0; i < count; i++)
-              {
-                  numbers[i] = other.numbers[i];
-              }    
+    NumbersContainer(const NumbersContainer& other)
+        :max_size(other.max_size),
+        count(other.count),
+        numbers(new value_type[max_size])
+    {
+        for (value_type i = 0; i < count; i++)
+        {
+            numbers[i] = other.numbers[i];
+        }
     }
 
-    NumbersContainer(NumbersContainer &&other)
-        : max_size(other.max_size),
-          count(other.count),
-          numbers(other.numbers){
-              other.numbers = nullptr;
+    NumbersContainer(NumbersContainer&& other)
+        :max_size(other.max_size),
+        count(other.count),
+        numbers(other.numbers)
+    {
+        other.numbers = nullptr;
     }
 
-    NumbersContainer& operator=(const NumbersContainer &other){
+    NumbersContainer& operator=(const NumbersContainer& other)
+    {
         return *this = NumbersContainer(other);
     }
 
-    NumbersContainer& operator=(NumbersContainer&& other){
-        if (numbers){
+    NumbersContainer& operator=(NumbersContainer&& other)
+    {
+        if (numbers)
+        {
             delete[] numbers;
         }
         max_size = other.max_size;
         count = other.count;
         numbers = other.numbers;
         other.numbers = nullptr;
-        return *this;   
+        return *this;
     }
 
-    void add(value_type value){
+    void add(value_type value)
+    {
         numbers[count] = value;
         count++;
     }
 
-    void print_content(){
+    void deleteValue(value_type value)
+    {
         for (size_type i = 0; i < count; i++)
         {
-            std::cout << numbers[i] << " \n";
+            if (numbers[i] == value)
+            {
+                numbers[i] = numbers[count - 1];
+                count--;
+                std::printf("Value: %i found at possition: %i and delete. \n", value, i);
+                return;
+            }
         }
-        
+        std::printf("Value: %i not found \n", value);
     }
-    private:
+
+    void deleteExactPosition(size_type position)
+    {
+        if (position > max_size || position < 0)
+        {
+            std::printf("Position %i is bigger or lower than size of array \n", position);
+        } else if (position > count)
+        {
+            std::printf("Position %i is greater than the last item position \n", position);
+        } else
+        {
+            deleteValue(numbers[position]);
+        }
+    }
+
+    void print_content()
+    {
+        for (size_type i = 0; i < count; i++)
+        {
+            std::printf("%i: %i \n", i, numbers[i]);
+        }
+    }
+
+private:
     size_type max_size;
     size_type count;
-    value_type *numbers;
+    value_type* numbers;
 };
 
-int main() {
+int main()
+{
     NumbersContainer containerA(10);
     containerA.add(1);
     containerA.add(2);
     containerA.add(3);
 
-    std::cout << "Kontener A org: \n";
+    std::cout << "ContainerA: \n";
     containerA.print_content();
     std::cout << std::endl;
-    /** Konstruktor kopiujacy oraz coppy operator
-     * / NumbersContainer container_copy(containerA); /
-     * / NumbersContainer container_copy = containerA; /
+
+    /**
+     * Copy constructor - test
      */
     NumbersContainer container_copy = containerA;
     container_copy.add(21);
-    std::cout << "Kontener skopiowany z dodanym 21: \n";
+    std::cout << "Container copied from: \"containerA\" and add \"21\" \n";
     container_copy.print_content();
     std::cout << std::endl;
 
+    /**
+     * Move constructor and delete functions - test
+     */
 
-    /** Konstruktor przenoszący 
-     * Przy przenoszeniu na obiekcie przenoszonym wywoływany jest destruktor.
-     * std::move z biblioteki standardowej robi dokładnie to samo 
-     * / NumbersContainer containerB(std::move(containerA)); /
-     * / static_cast<NumbersContainer&&>(containerA) /
-     * 
-     * Operator przypisania przenoszący oraz wywolanie konstruktora
-     * /NumbersContainer container_move(std::move(containerA)); /
-     * /NumbersContainer container_move = std::move(container_copy); /
-     */ 
-   
     NumbersContainer container_move = std::move(containerA);
-    std::cout << "Kontener move z dodanym 11: \n";
+    std::cout << "Container moved from: \"containerA\" and add \"11\" \n";
     container_move.add(11);
     container_move.print_content();
     std::cout << std::endl;
+
+    std::cout << "Container_move after delete possition 0 and try delete value \"1\" \n";
+    container_move.deleteExactPosition(0);
+    container_move.deleteValue(1);
+    container_move.print_content();
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+**LearningC++**
+*Hello, this repository is used for me to learn cpp and git itself. I am uploading here issues that I am currently practicing.*
+
+**Oop Basics**
+1. [Rule of five](https://github.com/Ultimaa/CPP_Basics_Learning/blob/master/OopBasics/ruleOf5.cpp) - Create copy constructors, move constructors, and assignment operators.
